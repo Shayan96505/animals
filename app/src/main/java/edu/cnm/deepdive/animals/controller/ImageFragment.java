61016676a8c4f17f6d91e10347f9ca4c38d64286
Shayan <sgolafshani@cnm.edu>
2020-10-07 10:25:32 -0600

    Replaced Thread with Async Task

--- app/src/main/java/edu/cnm/deepdive/animals/controller/ImageFragment.java
+++ app/src/main/java/edu/cnm/deepdive/animals/controller/ImageFragment.java
@@ -1,5 +1,6 @@
 package edu.cnm.deepdive.animals.controller;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.webkit.WebResourceRequest;
@@ -18,6 +19,7 @@ import edu.cnm.deepdive.animals.model.ApiKey;
 import edu.cnm.deepdive.animals.service.AnimalService;
 import java.io.IOException;
 import java.util.List;
+import java.util.Objects;
 import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
@@ -42,55 +44,67 @@ public class ImageFragment extends Fragment {
         return false;
       }
     });
-  WebSettings settings = contentView.getSettings();
-  settings.setJavaScriptEnabled(true);
-  settings.setSupportZoom(true);
-  settings.setBuiltInZoomControls(true);
-  settings.setDisplayZoomControls(false);
-  settings.setUseWideViewPort(true);
-  settings.setLoadWithOverviewMode(true);
-  new Retriever().start();
-}
+    WebSettings settings = contentView.getSettings();
+    settings.setJavaScriptEnabled(true);
+    settings.setSupportZoom(true);
+    settings.setBuiltInZoomControls(true);
+    settings.setDisplayZoomControls(false);
+    settings.setUseWideViewPort(true);
+    settings.setLoadWithOverviewMode(true);
+    new RetrieverTask().execute();
+  }
 
-  private class Retriever extends Thread {
+  private class RetrieverTask extends AsyncTask<Void, Void, List<Animal>> {
+
+    private AnimalService animalService;
+
 
     @Override
-    public void run() {
+    protected void onPreExecute() {
+      super.onPreExecute();
       Gson gson = new GsonBuilder()
           .create();
-
       Retrofit retrofit = new Retrofit.Builder()
           .baseUrl("https://us-central1-apis-4674e.cloudfunctions.net/")
           .addConverterFactory(GsonConverterFactory.create(gson))
           .build();
-
-      AnimalService animalService = retrofit.create(AnimalService.class);
-
-      try {
-
-        Response<ApiKey> keyResponse = animalService.getApiKey().execute();
-        ApiKey key = keyResponse.body();
-        assert key != null;
-        final String clientKey = key.getKey();
-
-        Response<List<Animal>> listResponse = animalService.getAnimals(clientKey).execute();
-        List<Animal> animalList = listResponse.body();
-        assert animalList != null;
-        final String imageUrl = animalList.get(0).getImageUrl();
-
-        getActivity().runOnUiThread(new Runnable() {
-          @Override
-          public void run() {
-            contentView.loadUrl(imageUrl);
-            //TODO implement webview loadURL method
-
-          }
-        });
-
-      } catch (IOException e) {
-        Log.e("AnimalService", e.getMessage(), e);
-      }
+      animalService = retrofit.create(AnimalService.class);
     }
 
+    @Override
+    protected List<Animal> doInBackground(Void... voids) {
+
+      try
+    {
+      Response<ApiKey> keyResponse = animalService.getApiKey().execute();
+      ApiKey key = keyResponse.body();
+      assert key != null;
+      final String clientKey = key.getKey();
+
+      Response<List<Animal>> listResponse = animalService.getAnimals(clientKey).execute();
+      List<Animal> animalList = listResponse.body();
+      assert animalList != null;
+      return animalList;
+    } catch(
+    IOException e)
+    {
+      Log.e("AnimalService", e.getMessage(), e);
+      cancel(true);
+    }
+      return null;
+    }
+
+    @Override
+    protected void onPostExecute(List<Animal> animalList) {
+      final String imageUrl = animalList.get(22).getImageUrl();
+
+      Objects.requireNonNull(getActivity()).runOnUiThread(new Runnable() {
+        @Override
+        public void run() {
+          contentView.loadUrl(imageUrl);
+
+        }
+      });
+    }
   }
 }
